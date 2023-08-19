a79d8201e57e1c4d6e2bc9ba12e28f5159cf2275
Ellina Babaian <famogl375@gmail.com>
2023-08-19 13:23:44 -0700

    Developing Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import HeaderContainer from "./components/header/HeaderContainer";
 
 function App() {
   return (
     <div className="App">
-      {/* <HeaderContainer/> */}
-      {/* <NavBar /> */}
+      <HeaderContainer/>
+      {/* <NavBarContainer /> */}
       {/* <Routes>
-        <Route path="/hotel" element={<Home />}></Route>
-        <Route path="/flights" element={<Flights />}></Route>
-        <Route path="/car" element={<Car />}></Route>
-        <Route path="/tours" element={<Tours />}></Route>
+      <Route path="/" element={<HomeContainer />}></Route>
+        <Route path="/hotel" element={<HotelsContainer />}></Route>
+        <Route path="/flights" element={<FlightsContainer />}></Route>
+        <Route path="/car" element={<CarContainer />}></Route>
+        <Route path="/tours" element={<ToursContainer />}></Route>
       </Routes> */}
     </div>
   );
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import "../../draftSTyle/main.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://www.trillo.io/wp-content/uploads/2021/05/Trillo-600-200.png"
+        alt="trillo logo"
+        className="logo"
+      />
+
+      <form action="#" className="search">
+        <input
+          type="text"
+          className="search__input"
+          placeholder="Search hotels"
+        />
+        <button className="search__button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </button>
+      </form>
+
+      <nav className="user-nav">
+        <div className="user-nav__icon-box">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            className="bi bi-chat-text"
+            viewBox="0 0 16 16"
+          >
+            <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
+            <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z" />
+          </svg>
+          <span className="user-nav__notification">7</span>
+        </div>
+
+        <div className="user-nav__icon-box">
+          <svg
+            width="24"
+            height="24"
+            strokeWidth="1.5"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M18.1336 11C18.7155 16.3755 21 18 21 18H3C3 18 6 15.8667 6 8.4C6 6.70261 6.63214 5.07475 7.75736 3.87452C8.88258 2.67428 10.4087 2 12 2C12.3373 2 12.6717 2.0303 13 2.08949"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M19 8C20.6569 8 22 6.65685 22 5C22 3.34315 20.6569 2 19 2C17.3431 2 16 3.34315 16 5C16 6.65685 17.3431 8 19 8Z"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M13.73 21C13.5542 21.3031 13.3019 21.5547 12.9982 21.7295C12.6946 21.9044 12.3504 21.9965 12 21.9965C11.6496 21.9965 11.3054 21.9044 11.0018 21.7295C10.6982 21.5547 10.4458 21.3031 10.27 21"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <span className="user-nav__notification">13</span>
+        </div>
+
+        <div className="user-nav__user">
+          <img
+            src="img/account.jpg"
+            alt="User photo"
+            className="user-nav__user-photo"
+          />
+          <span className="user-nav__user-name">Ellina</span>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
