7c05ad67dcd67b498a7a36cd3e22533faba38f0e
Ellina Babaian <famogl375@gmail.com>
2023-08-19 13:07:27 -0700

    Setting up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <HeaderContainer/> */}
+      {/* <NavBar /> */}
+      {/* <Routes>
+        <Route path="/hotel" element={<Home />}></Route>
+        <Route path="/flights" element={<Flights />}></Route>
+        <Route path="/car" element={<Car />}></Route>
+        <Route path="/tours" element={<Tours />}></Route>
+      </Routes> */}
     </div>
   );
 }
