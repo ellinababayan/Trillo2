ecacc0724e20c6497895010edae5305e6260d406
Ellina Babaian <famogl375@gmail.com>
2023-08-20 15:40:12 -0700

    Home page styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import "./Home.css";
+// import "../../../public/Trillo-600-200.png"
 
 const Home = () => {
   return (
     <div className="home_container">
+      <div className="summary_background"></div>
       <div className="summary block">
-        <h1>Welcome to Trillo - Your Travel Planning Companion</h1>
+        <img
+          src="https://www.trillo.io/wp-content/uploads/2021/05/Trillo-600-200.png"
+          alt="trillo logo"
+          className="logo_big"
+        />
+        <h1> Your Travel Planning Companion</h1>
         <h4>Explore the World with Ease and Excitement</h4>
-        <p>
-          At Trillo, we understand that every journey is unique, and your travel
-          planning experience should reflect that. Whether you're jet-setting
-          across the globe or embarking on a local adventure, Trillo is here to
-          transform your travel dreams into reality.
-        </p>
+        <div>
+          <a href="/hotel" className="navigation">
+            <span>Hotel</span>
+          </a>
+          <a href="#" className="navigation">
+            <span>Flight</span>
+          </a>
+          <a href="#" className="navigation">
+            <span>Car Rental</span>
+          </a>
+          <a href="#" className="navigation">
+            <span>Tours</span>
+          </a>
+        </div>
       </div>
-
+      
       <div className="why block">
         <h2>Why Choose Trillo?</h2>
         <span>
@@ -49,18 +64,20 @@ const Home = () => {
         <h2>
           Ready to Dive In? <br></br>Let's Get Started!
         </h2>
-        <a href="/hotel" className="navigation">
-          <span>Hotel</span>
-        </a>
-        <a href="#" className="navigation">
-          <span>Flight</span>
-        </a>
-        <a href="#" className="navigation">
-          <span>Car Rental</span>
-        </a>
-        <a href="#" className="navigation">
-          <span>Tours</span>
-        </a>
+        <div>
+          <a href="/hotel" className="navigation">
+            <span>Hotel</span>
+          </a>
+          <a href="#" className="navigation">
+            <span>Flight</span>
+          </a>
+          <a href="#" className="navigation">
+            <span>Car Rental</span>
+          </a>
+          <a href="#" className="navigation">
+            <span>Tours</span>
+          </a>
+        </div>
       </div>
     </div>
   );
