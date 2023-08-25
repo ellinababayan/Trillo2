04c499e2f81643ae960ca23fe91a220bd2d704c3
Ellina Babaian <famogl375@gmail.com>
2023-08-24 22:11:29 -0700

    Tours Page

--- src/pages/tours/Tours.jsx
+++ src/pages/tours/Tours.jsx
@@ -1,12 +1,67 @@
 import React from "react";
-import "./Tours.css"
+import "./Tours.css";
 
-const Tours = () => {
-return (
+const Tours = (props) => {
+  return (
     <div className="tours_container">
-        <div>Tours</div>
+      <div className="tours_container">
+        <div>
+          <div className="container__searchbar">
+            <h1>Tours Search</h1>
+            <form onSubmit={props.handleSubmit}>
+              <label htmlFor="departure">Departure City:</label>
+              <input
+                type="text"
+                id="departure"
+                value={props.departure}
+                className="search__tours"
+                onChange={(e) => props.setDeparture(e.target.value)}
+                required
+              />
+              <label htmlFor="destination">Destination:</label>
+              <input
+                type="text"
+                id="destination"
+                value={props.destination}
+                className="search__tours"
+                onChange={(e) => props.setDestination(e.target.value)}
+                required
+              />
+              <label htmlFor="dates">Dates:</label>
+              <input
+                type="date"
+                id="dates"
+                value={props.dates}
+                className="search__tours"
+                onChange={(e) => props.setDates(e.target.value)}
+                required
+              />
+              <button type="submit" className="btn find">
+                Search tours
+              </button>
+            </form>
+            <div className="results">
+              <p>Book Tour:</p>
+              {props.bookingServices.map((service, index) => (
+                <a
+                  key={index}
+                  href={service}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    className="logo_tours"
+                    src={props.bookingServiceLogos[index]}
+                    alt={`Booking Service ${service + 1}`}
+                  ></img>
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-)
-}
+  );
+};
 
-export default Tours
+export default Tours;
--- src/pages/tours/ToursContainer.jsx
+++ src/pages/tours/ToursContainer.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Tours from "./Tours";
 
-const ToursContainer = ( ) => {
-    return(<Tours />)
-}
+const ToursContainer = () => {
+  const [departure, setDeparture] = useState("");
+  const [destination, setDestination] = useState("");
+  const [dates, setDates] = useState("");
+  const [bookingServices, setBookingServices] = useState([]);
 
-export default ToursContainer
+  const bookingServiceLogos = [
+    "https://upload.wikimedia.org/wikipedia/commons/4/45/GetYourGuide_logo.png",
+    "https://logos-world.net/wp-content/uploads/2021/02/Expedia-Logo-2012-2021.png",
+  ];
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const bookingService1 = `https://www.getyourguide.com/-l1320/-tc35/?cmp=ga&cq_src=google_ads&cq_cmp=6654173655&cq_con=84269756582&cq_term=tours%20adventure%20tours&cq_med=&cq_plac=&cq_net=g&cq_pos=&cq_plt=gp&campaign_id=6654173655&adgroup_id=84269756582&target_id=kwd-333267410390&loc_physical_ms=9031140&match_type=b&ad_id=430559575647&keyword=tours%20adventure%20tours&ad_position=&feed_item_id=&placement=&device=c&partner_id=CD951&gclid=Cj0KCQjw_5unBhCMARIsACZyzS2n2c5Tor0D9DpwYaZsdfSfo22FNOI-9uqe3ckvmL_6DQUQUZYHd2AaAizJEALw_wcB`;
+    const bookingService2 = `https://www.expedia.com/Activities??from=${departure}&to=${destination}&dates=${dates}`;
+
+    setBookingServices([bookingService1, bookingService2]);
+  };
+  return (
+    <Tours
+      handleSubmit={handleSubmit}
+      departure={departure}
+      setDeparture={setDeparture}
+      dates={dates}
+      setDates={setDates}
+      destination={destination}
+      setDestination={setDestination}
+      bookingServices={bookingServices}
+      bookingServiceLogos={bookingServiceLogos}
+    />
+  );
+};
+
+export default ToursContainer;
